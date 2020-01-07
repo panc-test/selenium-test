95cd1206f55a43f1df0aaac3054c223f12e77463
panc-test <1976085712@qq.com>
2020-01-07 23:25:57 +0800

    '20200107'

--- 202001/20200105-01.py
+++ 202001/20200105-01.py
@@ -4,6 +4,7 @@ webdriver基本使用命令
 
 from selenium import webdriver  # 导入webdriver模块
 from time import *
+
 driver = webdriver.Chrome()  # 打开Google浏览器
 driver.get("https://www.baidu.com")  # 打开 网址
 # 打开本地 html页面
@@ -12,5 +13,6 @@ title=driver.title  # 获取打开网址 的名字
 url=driver.current_url  # 获取打开网址的 url
 print('title:',title)
 print('url:',url)
+
 sleep(5)
 driver.close() #关闭浏览器窗口
---
+++ 202001/20200107-01.py
@@ -0,0 +1,14 @@
+'''
+定位页面元素的方法
+'''
+
+from selenium import webdriver
+driver=webdriver.Chrome()
+driver.get('https://www.baidu.com/')
+
+# driver.find_element_by_id("kw").send_keys("web自动化")
+# driver.find_element_by_name("wd").send_keys("web自动化")
+# driver.find_element_by_css_selector("#kw").send_keys("web自动化")
+# driver.find_element_by_css_selector("#u1 > a:nth-child(1)").click()
+driver.find_element_by_xpath("//*[@id="kw"]").send_keys("web自动化")
+
