f44167df05485b92f72529769afbeff496dff53b
panc-test <1976085712@qq.com>
2020-01-22 16:46:15 +0800

    20200121 xlwt库练习-京东商城抓取手机型号和价格

--- 202001/20200121-01.py
+++ 202001/20200121-01.py
@@ -3,10 +3,11 @@
 '''
 from selenium import webdriver
 import xlwt
+import time
 
 driver=webdriver.Chrome()
 driver.get('https://search.jd.com/Search?keyword=%E6%89%8B%E6%9C%BA&enc=utf-8&wq=%E6%89%8B%E6%9C%BA&pvid=e424a92e8d0d4845944e22ecb9a79534')
-
+time.sleep(5)
 name=driver.find_elements_by_css_selector('div.p-name.p-name-type-2')
 price=driver.find_elements_by_css_selector('div.p-price')
 
